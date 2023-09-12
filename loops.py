f84d353ac64396f0e2f1e9323e5573dcf920efdf
igbojionu <igbojionu@gmail.com>
2023-09-12 11:43:45 +0100

    Loops Class

---
+++ loops.py
@@ -0,0 +1,36 @@
+#variables
+
+name = "chuks"
+phone = 7065633737
+car = "honda"
+
+
+#list
+
+marketItems = [" ","mango", 877,"Orange", "pea", 2, 23, 0.4]
+
+#Dictionaries
+
+student = {
+    'name': 'jane',
+    'phone': '+23470656555',
+    'Sex': 'Female',
+    'City': 'Port Harcourt'
+}
+
+#conditionals
+
+if (phone)== 300:
+    print(student)
+elif (phone)== 300:
+    print (marketItems)
+else:
+    print("End of code")
+
+#For loop
+
+for mkt in marketItems:
+    if type(mkt)==str:
+        print(mkt)
+
+
---
+++ whileloop.py
@@ -0,0 +1,19 @@
+letters = ['j', 'o', 'i', 'n']
+
+word = ''
+count = 0
+
+while count < len(letters):
+    word = word + letters[count]
+    count = count + 1
+    print ("counted")
+
+print (word)
+
+
+num = 0
+
+while num < 10:
+    num = num + 1
+    print ("yess number is les than 25")
+
