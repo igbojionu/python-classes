00948f9af66b64d0b3f4fdeda72a058b24eb7740
igbojionu <igbojionu@gmail.com>
2023-09-22 11:48:11 +0100

    push

--- excercises.py
+++ excercises.py
@@ -7,3 +7,5 @@ choice = random.choice(['win', 'lose', 'draw'])
 # Print the random number
 print(random_number)
 print (choice)
+#generate random number until 50tries the showa a win else keeps showing loose or draw
+#Examples¶ implement all Basic examples: in this url =  { https://docs.python.org/3/library/random.html?highlight=randint#examples }
