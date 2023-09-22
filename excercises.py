8556dec674cd68ee444efa6ee2a5de933a5872f6
igbojionu <igbojionu@gmail.com>
2023-09-22 11:44:43 +0100

    ass

---
+++ excercises.py
@@ -0,0 +1,9 @@
+import random
+
+# Generate a random number between 1 and 100
+random_number = random.randint(1, 100)
+choice = random.choice(['win', 'lose', 'draw'])
+
+# Print the random number
+print(random_number)
+print (choice)
