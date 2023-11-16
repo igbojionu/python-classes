f483b8b308366828a3d9b1dc1b60b5675a1031d6
igbojionu <igbojionu@gmail.com>
2023-11-16 11:54:05 +0100

    main

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 # Open a file for writing (creates the file if it doesn't exist)
-file = open("example.txt", "w")
+file = open("chuks.txt", "w")
 
 # Write content to the file
 file.write("Hello, this is a sample text.")
 file.close()  # Close the file
 
 # Open the file for reading
-file = open("example.txt", "r")
+file = open("chuks.txt", "r")
 
 # Read and print the file content
-content = file.read()
-print(content)
+#content = file.read()
+#print(content)
 
 # Close the file
 file.close()
